d540ad79f41b405010d3295e320ed01b68c178b4
konralitt <litwiniukkonrad01@gmail.com>
2026-07-16 14:35:33 +0200

    Read and print raw HTTP request

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,7 +9,13 @@ public class Main {
             ServerSocket ss = new ServerSocket(8080);
             System.out.println("Server started. Listening on port 8080");
             while (true) {
-                ss.accept();
+                try(Socket socket = ss.accept();BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
+                    String line;
+                    while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                        System.out.println(line);
+                    }
+                }
+
             }
         }
         catch(Exception e){

8350475e887e2807b0f058820d4475c0e1cbd8e3
konralitt <litwiniukkonrad01@gmail.com>
2026-07-28 12:36:59 +0200

    path-based routing with 404

--- src/Main.java
+++ src/Main.java
@@ -12,15 +12,20 @@ public class Main {
             while (true) {
                 try(Socket socket = ss.accept();BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));PrintWriter pw = new PrintWriter(socket.getOutputStream());) {
                     String line;
+                    String firstLine = reader.readLine();
+                    System.out.println(firstLine);
                     while ((line = reader.readLine()) != null && !line.isEmpty()) {
                         System.out.println(line);
                     }
-                    String html = "<h1>Hello from Java</h1>";
-                    pw.print("HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: text/html\r\n" +
-                            "Content-Length: "+html.getBytes(StandardCharsets.UTF_8).length + "\r\n" +
-                            "\r\n" + html
-                           );
+                    String[] splittedFirstLine = firstLine.split("[\\s]+");
+                    String response;
+                    if(splittedFirstLine[1].equals("/"))
+                        response = buildResponse("200 OK", "<h1>Hello from Java</h1>");
+                    else if(splittedFirstLine[1].equals("/about"))
+                        response = buildResponse("200 OK", "<h1>About</h1>");
+                    else
+                        response = buildResponse("404 Not Found", "<h1>404 Not Found</h1>");
+                    pw.print(response);
                     pw.flush();
                 }
 
@@ -29,5 +34,11 @@ public class Main {
         catch(Exception e){
             System.out.println("Server Error: " + e);
         }
+    }
+    public static String buildResponse(String status, String body){
+        return "HTTP/1.1 "+ status +"\r\n" +
+                "Content-Type: text/html\r\n" +
+                "Content-Length: "+ body.getBytes(StandardCharsets.UTF_8).length + "\r\n" +
+                "\r\n" + body;
     }
 }

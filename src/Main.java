b6fbc448e4fce00b92129cc4e04502feb3f74738
konralitt <litwiniukkonrad01@gmail.com>
2026-08-03 22:10:44 +0200

    read POST body via Content-Length

--- src/Main.java
+++ src/Main.java
@@ -12,23 +12,35 @@ public class Main {
             while (true) {
                 try(Socket socket = ss.accept();BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));PrintWriter pw = new PrintWriter(socket.getOutputStream());) {
                     String line;
+                    int cntLength = 0;
+                    String[] lengthSplited;
                     String firstLine = reader.readLine();
                     System.out.println(firstLine);
                     while ((line = reader.readLine()) != null && !line.isEmpty()) {
                         System.out.println(line);
+                        if(line.toLowerCase().contains("content-length")){
+                            lengthSplited = line.trim().split(":");
+                            cntLength = Integer.parseInt(lengthSplited[1].trim());
+                            System.out.println(cntLength);
+                        }
                     }
+                    char[] buffer = new char[cntLength];
+                    reader.read(buffer, 0, cntLength);
+                    String body = new String(buffer);
+                    System.out.println("BODY: " + body);
                     String[] splittedFirstLine = firstLine.split("[\\s]+");
                     String response;
                     if(splittedFirstLine[1].equals("/"))
                         response = buildResponse("200 OK", "<h1>Hello from Java</h1>");
                     else if(splittedFirstLine[1].equals("/about"))
                         response = buildResponse("200 OK", "<h1>About</h1>");
+                    else if(splittedFirstLine[1].startsWith("/links/"))
+                        response = buildResponse("200 OK", "Requested code: " + splittedFirstLine[1].substring("/links/". length()));
                     else
                         response = buildResponse("404 Not Found", "<h1>404 Not Found</h1>");
                     pw.print(response);
                     pw.flush();
                 }
-
             }
         }
         catch(Exception e){

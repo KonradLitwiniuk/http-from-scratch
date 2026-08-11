010cf1e083f144d4a1cc0aa9c7dbb6d462733e33
konralitt <litwiniukkonrad01@gmail.com>
2026-08-11 23:52:55 +0200

    Integrate JDBC repository into HTTP server with 302 redirect handling

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Locale;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +14,9 @@ public class Main {
             System.out.println("Starting server");
             ServerSocket ss = new ServerSocket(8080);
             System.out.println("Server started. Listening on port 8080");
+            System.out.println("Connecting to database...");
+            Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/scratch", "dev", "dev");
+            LinkRepository lp = new LinkRepository(con);
             while (true) {
                 try(Socket socket = ss.accept();BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));PrintWriter pw = new PrintWriter(socket.getOutputStream());) {
                     String line;
@@ -35,13 +43,27 @@ public class Main {
                     else if(splittedFirstLine[1].equals("/about"))
                         response = buildResponse("200 OK", "<h1>About</h1>");
                     else if(splittedFirstLine[1].startsWith("/links/"))
-                        response = buildResponse("200 OK", "Requested code: " + splittedFirstLine[1].substring("/links/". length()));
+                    {
+                        String code = splittedFirstLine[1].substring("/links/". length());
+                        Optional<Link> link = lp.findByCode(code);
+                        if(link.isPresent())
+                        {
+                            response = buildRedirectResponse("302 Found", link.get().getUrl());
+                        }
+                        else
+                        {
+                            response = buildResponse("404 Not Found", "<h1>Link not found</h1>");
+                        }
+                    }
                     else
-                        response = buildResponse("404 Not Found", "<h1>404 Not Found</h1>");
+                         response = buildResponse("404 Not Found", "<h1>404 Not Found</h1>");
                     pw.print(response);
                     pw.flush();
                 }
             }
+        }
+        catch(SQLException e){
+            System.out.println("Server Error: " + e);
         }
         catch(Exception e){
             System.out.println("Server Error: " + e);
@@ -52,5 +74,10 @@ public class Main {
                 "Content-Type: text/html\r\n" +
                 "Content-Length: "+ body.getBytes(StandardCharsets.UTF_8).length + "\r\n" +
                 "\r\n" + body;
+    }
+    public static String buildRedirectResponse(String status, String Location)
+    {
+        return "HTTP/1.1 " + status + "\r\n" +
+                "Location: " + Location + "\r\n" + "\r\n";
     }
 }
